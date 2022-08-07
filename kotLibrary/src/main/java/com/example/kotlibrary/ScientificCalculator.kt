5398bd11aba86bcc355e1663a40a7785c5a41e0c
Brad Veenvliet <bveenvliet@gmail.com>
2022-08-07 18:01:46 -0400

    add ScientificCalculator.kt

---
+++ kotLibrary/src/main/java/com/example/kotlibrary/ScientificCalculator.kt
@@ -0,0 +1,16 @@
+package com.example.kotlibrary
+
+import kotlin.math.sqrt
+
+class ScientificCalculator {
+
+    fun squareRoot(a: Double): Double {
+        return sqrt(a)
+    }
+
+    fun cubeRoot(a: Double): Double {
+        return Math.cbrt(a)
+    }
+
+}
+
