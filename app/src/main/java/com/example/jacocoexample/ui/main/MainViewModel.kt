c2b2a77209e944168c618b9b222275e13879ed84
chuxizhang <chuxi-zhang@outlook.com>
2022-08-11 13:44:59 -0400

    add editText and onClickListener for the button

--- app/src/main/java/com/example/jacocoexample/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/jacocoexample/ui/main/MainViewModel.kt
@@ -1,10 +1,15 @@
 package com.example.jacocoexample.ui.main
 
 import androidx.lifecycle.ViewModel
+import com.example.kotlibrary.Calculator
 
 class MainViewModel : ViewModel() {
 
     fun checkAddition() {
         println("asdasd")
+    }
+
+    fun add(a: Int, b: Int): Int {
+        return Calculator().add(a, b)
     }
 }
